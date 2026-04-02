ddfd418e2512e2513903f835d64ab12c74202086
Cesar <58753718+Ces-D@users.noreply.github.com>
2026-04-02 14:41:07 -0400

    chore(blueprint-markdown): add detailed debug logging for markdown interpretation
    Add granular debug logs for markdown tags and events to aid troubleshooting of the MarkdownInterpreter behavior.

--- crates/blueprint/interpreter/markdown.rs
+++ crates/blueprint/interpreter/markdown.rs
@@ -32,6 +32,7 @@ impl MarkdownInterpreter {
     fn handle_tag_start(&mut self, tag: &Tag) -> Result<()> {
         match tag {
             Tag::Paragraph => {
+                log::debug!("Tag start: Paragraph");
                 self.builder.reset_styles();
                 Ok(())
             }
@@ -41,6 +42,7 @@ impl MarkdownInterpreter {
                 classes: _,
                 attrs: _,
             } => {
+                log::debug!("Tag start: Heading level {:?}", level);
                 let level = match level {
                     pulldown_cmark::HeadingLevel::H1 => 1,
                     pulldown_cmark::HeadingLevel::H2 => 2,
@@ -52,16 +54,19 @@ impl MarkdownInterpreter {
                 super::block_adornment::set_heading_style(level, &mut self.builder)
             }
             Tag::BlockQuote(_) | Tag::CodeBlock(_) => {
+                log::debug!("Tag start: BlockQuote or CodeBlock");
                 self.builder.new_line();
                 self.builder.reset_styles();
                 self.builder.set_is_bold(true);
                 Ok(())
             }
             Tag::List(ordered_start) => {
+                log::debug!("Tag start: List (ordered_start={:?})", ordered_start);
                 self.list_index = *ordered_start;
                 Ok(())
             }
             Tag::Item => {
+                log::debug!("Tag start: Item (list_index={:?})", self.list_index);
                 let before = match self.list_index {
                     Some(i) => {
                         let mut b = ListItemBefore::new_ordered(None);
@@ -73,10 +78,14 @@ impl MarkdownInterpreter {
                 before.to_builder_command(&mut self.builder)
             }
             Tag::Strong => {
+                log::debug!("Tag start: Strong");
                 self.builder.set_is_bold(true);
                 Ok(())
             }
-            _ => Ok(()),
+            _ => {
+                log::debug!("Tag start: unhandled {:?}", tag);
+                Ok(())
+            }
         }
     }
 
@@ -84,26 +93,56 @@ impl MarkdownInterpreter {
         for event in Parser::new_ext(markdown, Options::ENABLE_TASKLISTS) {
             match &event {
                 pulldown_cmark::Event::Start(tag) => self.handle_tag_start(tag),
-                pulldown_cmark::Event::End(_) => {
+                pulldown_cmark::Event::End(tag) => {
+                    log::debug!("Event: End({:?})", tag);
                     self.builder.new_line();
                     continue;
                 }
-                pulldown_cmark::Event::Text(cow_str) => self.builder.add_content(cow_str),
-                pulldown_cmark::Event::Code(_)
-                | pulldown_cmark::Event::InlineMath(_)
-                | pulldown_cmark::Event::DisplayMath(_)
-                | pulldown_cmark::Event::Html(_)
-                | pulldown_cmark::Event::InlineHtml(_)
-                | pulldown_cmark::Event::FootnoteReference(_) => continue,
-                pulldown_cmark::Event::SoftBreak | pulldown_cmark::Event::HardBreak => {
+                pulldown_cmark::Event::Text(cow_str) => {
+                    log::debug!("Event: Text(\"{}\")", cow_str);
+                    self.builder.add_content(cow_str)
+                }
+                pulldown_cmark::Event::Code(code) => {
+                    log::debug!("Event: Code(\"{}\")", code);
+                    continue;
+                }
+                pulldown_cmark::Event::InlineMath(math) => {
+                    log::debug!("Event: InlineMath(\"{}\")", math);
+                    continue;
+                }
+                pulldown_cmark::Event::DisplayMath(math) => {
+                    log::debug!("Event: DisplayMath(\"{}\")", math);
+                    continue;
+                }
+                pulldown_cmark::Event::Html(html) => {
+                    log::debug!("Event: Html(\"{}\")", html);
+                    continue;
+                }
+                pulldown_cmark::Event::InlineHtml(html) => {
+                    log::debug!("Event: InlineHtml(\"{}\")", html);
+                    continue;
+                }
+                pulldown_cmark::Event::FootnoteReference(label) => {
+                    log::debug!("Event: FootnoteReference(\"{}\")", label);
+                    continue;
+                }
+                pulldown_cmark::Event::SoftBreak => {
+                    log::debug!("Event: SoftBreak");
+                    self.builder.new_line();
+                    continue;
+                }
+                pulldown_cmark::Event::HardBreak => {
+                    log::debug!("Event: HardBreak");
                     self.builder.new_line();
                     continue;
                 }
                 pulldown_cmark::Event::Rule => {
+                    log::debug!("Event: Rule");
                     let r = HorizontalRule::new();
                     r.to_builder_command(&mut self.builder)
                 }
                 pulldown_cmark::Event::TaskListMarker(checked) => {
+                    log::debug!("Event: TaskListMarker(checked={})", checked);
                     let before = TaskListBefore::new(*checked);
                     before.to_builder_command(&mut self.builder)
                 }
